6dcb60234d0fecb00a5777d8c5b33bc31888252d
孙华 <33270652@qq.com>
2017-06-03 10:31:14 +0800

    mark copyright
    quark by ihua.win

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 </div>
-<div id="f">&copy; 2017 Powered by Typecho & Theme Quark</div>
+<div id="f">&copy; <?php echo date('Y');?> Powered by Typecho & Theme <a href="http://ihua.win" target="_blank">Quark</a></div>
 <?php $this->footer(); ?>
 </body>
 </html>
